4181e32101133cc1c0e7c9ba35f13c865c68266a
Aaron Jones <aaronjones@AirmetSierra.local>
2018-03-25 23:04:06 -0400

    Javascript Time
    some notes

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+
+
+// var firstName = prompt("What is your name?");
+// var lastLame = prompt("What is your last name")
+// var userAge = prompt("What is your age")
+// var fullName = firstName + lastLame
+// console.log("Hi there " + " " + fullName)
+// console.log(firstName + " is " + userAge + " years old")
+
+var userAge = prompt("What is your age");
+var time = userAge * 365.25;
+console.log("You've been alive " + time + " days.");
+
+// assuming x = 5
+// x === "5"  false
+// x !== "5" true
+
+
