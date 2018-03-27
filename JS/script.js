f795a50963128f93f9161bf1391e2ae24b0c20a5
Aaron Jones <aaronjones@AirmetSierra.local>
2018-03-26 23:30:22 -0400

    JS updaates

--- JS/script.js
+++ JS/script.js
@@ -7,12 +7,27 @@
 // console.log("Hi there " + " " + fullName)
 // console.log(firstName + " is " + userAge + " years old")
 
-var userAge = prompt("What is your age");
-var time = userAge * 365.25;
-console.log("You've been alive " + time + " days.");
+// var userAge = prompt("What is your age");
+// var time = userAge * 365.25;
+// console.log("You've been alive " + time + " days.");
 
 // assuming x = 5
 // x === "5"  false
 // x !== "5" true
 
 
+var number = 5;
+
+var guess = prompt("guess a number");
+
+if(Number(guess) === number) {
+alert("You guesssed it");
+}
+
+else if(Number(guess) < number) {
+alert("too low");
+}
+
+else {
+alert("too high");
+}
