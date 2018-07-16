8110bf201fb2ef24035d52d7b983644223769bd9
Aaron Jones <aaronjones@AirmetSierra.local>
2018-07-16 08:48:49 -0400

    Setting Up Outline

--- JS/ToDo/assets/js/todo.js
+++ JS/ToDo/assets/js/todo.js
@@ -1,7 +1,19 @@
 console.log("TODO.JS CONNECTED")
 
 //check off specific todos by clicking
+var complete = false;
 
 $("li").click(function() {
-	console.log("clicked an li")
-})
+	if($(this).css("color") === "rgb(128, 128, 128)"){
+		$(this).css({
+			color: "black"
+			textDecoration = "none"
+		});
+	}
+	else {
+		$(this).css({
+			color: "gray",
+			textDecoration: "line-through"
+		});
+	}
+});
