6d384ab3c4f5a5c11a6ad4bac226d808ccaf309f
Aaron Jones <aaronjones@AirmetSierra.attlocal.net>
2019-11-14 18:40:07 -0500

    Secrets.js (121 JS Stalker)
    Following along, again, with the web class)

---
+++ MiscPractice/secrets.js
@@ -0,0 +1,12 @@
+// secrets.js connects to secrets.html with parent of practice.html
+console.log("secrets.js is connected");
+
+var firstName = prompt("Enter first name: ");
+var lastName = prompt("Enter last name: ");
+var age = prompt("Enter your age: ");
+
+var fullName = firstName + " " + lastName;
+
+console.log("Your name is " + fullName + " and your age is " + age);
+
+
