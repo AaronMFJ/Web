7376374f5ab8cff020bcb884d12942e1e8d8fbe5
Aaron Jones <aaronjones@AirmetSierra.attlocal.net>
2019-11-14 19:01:19 -0500

    Finished Unit 121
    Just added a age calculator

--- MiscPractice/secrets.js
+++ MiscPractice/secrets.js
@@ -4,9 +4,12 @@ console.log("secrets.js is connected");
 var firstName = prompt("Enter first name: ");
 var lastName = prompt("Enter last name: ");
 var age = prompt("Enter your age: ");
+var daysAlive = age * 365.25;
 
 var fullName = firstName + " " + lastName;
 
 console.log("Your name is " + fullName + " and your age is " + age);
+alert("You have been alive ~" + daysAlive + " days.");
+
 
 
