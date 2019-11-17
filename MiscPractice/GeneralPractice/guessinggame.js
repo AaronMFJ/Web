ed5d66fe945ee01601a84c7e586262ec67c19d25
AaronMFJ <parasiteartist@gmail.com>
2019-11-17 16:53:07 -0500

    133.3: While loops. Combined all games to 1
    Combined all the other previous exercises into one, then make user select which one to practice. Also added while loops practice and refactored all games to use first portion of name and age entry

--- MiscPractice/GeneralPractice/guessinggame.js
+++ MiscPractice/GeneralPractice/guessinggame.js
@@ -1,31 +1,102 @@
 console.log("guessingGame.js is connected!");
 
+//setup info for games
+var firstName = prompt("Enter first name: ");
+var lastName = prompt("Enter last name: ");
+var fullName = firstName + " " + lastName;
+var age = Number(prompt("Enter your age: "));
 
-//create secret number
+//Select a game
+var stringChoice = prompt("Select game number: 1-guessing, 2-whileloops, 3-AgeVerification ");
+var numChoice = Number(stringChoice);
+
+//create secret number for games
 var number = 10;
 console.log(number);
 
-//ask user for a guess
-var stringGuess = prompt("Guess a number: ");
-var numGuess = Number(stringGuess);
-console.log(numGuess, number);
+//1: Guessing Game. Ask user for a guess.
+if(numChoice === 1){
+	var stringGuess = prompt("Guess a number: ");
+	var numGuess = Number(stringGuess);
+	console.log(numGuess, number);
+	// func(guess){
+	// 	var numGuess = alert("Guess a number: ")
+	// };
 
-// func(guess){
-// 	var numGuess = alert("Guess a number: ")
-// };
+	//check the guess
+	if(numGuess === number) {
+		console.log("you guess correctly");
+		alert("you guessed correctly!");
+	}
 
-//check the guess
+	else if(numGuess > number) {
+		alert("your guess is too high!");
+	}
 
-if(Number(numGuess) === number) {
-	console.log("you guess correctly");
-	alert("you guessed correctly!");
+	else {
+		alert("your guess is too low!");
+	}
 }
 
-else if(Number(numGuess > number)) {
-	alert("your guess is too high!");
+//2: While Loops.
+else if(numChoice === 2){
+	var count = 0;
+	// var strName = prompt("enter your name: ");
+	// var numName = Number(strName);
+
+	//remind user of name
+	alert("You said your name was " + fullName);
+	console.log("Length of name: " + fullName.length);
+
+	//display name, letter by letter
+	while(count < fullName.length){
+		console.log(fullName[count]);
+		count++;
+	}
+	//display ran number, in increments of 5
+	while(number > 0) {
+		console.log("rand number: " + number);
+		number-=5;
+	}
 }
 
-else {
-	alert("your guess is too low!");
-}
+//3. Drinking Age Guessing
+else if(numChoice === 3) {
+	// var firstName = prompt("Enter first name: ");
+	// var lastName = prompt("Enter last name: ");
+	// var age = Number(prompt("Enter your age: "));
+	var daysAlive = age * 365.25;
 
+	var fullName = firstName + " " + lastName;
+
+	console.log("Your name is " + fullName + " and your age is " + age);
+	alert("You have been alive ~" + daysAlive + " days.");
+
+	if(age % 2 === 1){
+		console.log("Your age is odd.");
+	}
+
+	if(age % Math.sqrt(age) === 0){
+		console.log("Perfect Square");
+	}
+
+	if(age < 18){
+		if(age < 0){
+			console.log("ERROR: Age is negative.");
+		}
+		else{
+			console.log("You cannot enter.");
+		}
+	}
+
+	else if(age < 21){
+		console.log("You may enter but cannot drink.");
+	}
+
+	else{
+		if(age === 21){
+			console.log("Happy 21st birthday!");
+		}
+		console.log("Welcome. Drinking age verified.");
+	}
+}
