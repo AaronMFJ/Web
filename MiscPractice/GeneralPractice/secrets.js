061dc2b619e1526031953bce61849a3771b23bb1
AaronMFJ <aaronmfj@live.com>
2019-11-17 15:12:28 -0500

    Logic Lesson

--- MiscPractice/GeneralPractice/secrets.js
+++ MiscPractice/GeneralPractice/secrets.js
@@ -3,13 +3,42 @@ console.log("secrets.js is connected");
 
 var firstName = prompt("Enter first name: ");
 var lastName = prompt("Enter last name: ");
-var age = prompt("Enter your age: ");
+var age = Number(prompt("Enter your age: "));
 var daysAlive = age * 365.25;
 
 var fullName = firstName + " " + lastName;
 
 console.log("Your name is " + fullName + " and your age is " + age);
 alert("You have been alive ~" + daysAlive + " days.");
+
+if(age % 2 === 1){
+	console.log("Your age is odd.");
+}
+
+if(age % Math.sqrt(age) === 0){
+	console.log("Perfect Square");
+}
+
+if(age < 18){
+	if(age < 0){
+		console.log("ERROR: Age is negative.");
+	}
+	else{
+		console.log("You cannot enter.");
+	}
+}
+
+else if(age < 21){
+	console.log("You may enter but cannot drink.");
+}
+
+else{
+	if(age === 21){
+		console.log("Happy 21st birthday!");
+	}
+	console.log("Welcome. Drinking age verified.");
+
+}
 
 
 
