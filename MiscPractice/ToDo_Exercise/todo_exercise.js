afab57df571c0d333b36e7f33653ad4c5cfa43eb
AaronMFJ <parasiteartist@gmail.com>
2020-01-19 14:25:54 -0500

    edits
    df

--- MiscPractice/ToDo_Exercise/todo_exercise.js
+++ MiscPractice/ToDo_Exercise/todo_exercise.js
@@ -17,6 +17,8 @@ function todoMaker() {
     console.log(`running todoMaker()`);
     let enterTodo = prompt(`Enter a todo.`);
     toDoArray.push(enterTodo);
+    console.log(`Adding ${enterTodo} to the list.`);
+    
     // userAnswer();
 } 
 
@@ -38,20 +40,24 @@ userAnswer = prompt(`What would you like to do? `);
     }
     else if(userAnswer === `list`) {
         console.log(toDoArray);
-        toDoArray.forEach(function(logArrayList){
-            console.log(`Todo list: ${logArrayList}`);
+        toDoArray.forEach(function(logArrayList, i){
+            // console.log(i + `: ` + logArrayList);
+            console.log(`${i}: ${logArrayList}`);
         });
         // userAnswer(); //need to find a way to not exit function
     }
     else if(userAnswer === `delete`) {
         //get thing to delete from user (picktoDelete)
         let pickToDelete = prompt(`What do you want to delete? ${toDoArray}`);
+        // let pickIndex = prompt(`What number do you want to delete? ${toDoArray}`);
         //for each item in array, deleteMatches is the hold for each item
         toDoArray.forEach(function(deleteMatches) {
             if(pickToDelete === deleteMatches) {
                 console.log(`DELETE: ${pickToDelete} matches ${deleteMatches}. DELETING INDEX: ${toDoArray.indexOf(pickToDelete)}`);
                 //find index location of the user selection, and only remove that 1
                 toDoArray.splice(toDoArray.indexOf(pickToDelete), 1);
+                //alternate version: delete index provided by user
+                // toDoArray.splice(pickIndex, 1)
             }
         });
     }
