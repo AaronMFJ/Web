436666cd1c938c2ae5cdec7e8a37fb6ef92b88cd
AaronMFJ <parasiteartist@gmail.com>
2020-01-29 09:58:27 -0500

    Update todo_exercise.js

--- MiscPractice/ToDo_Exercise/todo_exercise.js
+++ MiscPractice/ToDo_Exercise/todo_exercise.js
@@ -46,6 +46,75 @@ function deleteToDo() {
     });
 }
 
+function printReverse(inputArray) {
+    console.log(`starting printReverse`);
+    for(var i = inputArray.length - 1; i >=0; i--) {
+        console.log(inputArray[i]);
+    }
+    // let arrayLength = inputArray.length - 1
+    // console.log(arrayLength);
+    // while (arrayLength != 0) {
+    //     // console.log(`print array` + inputArray.indexOf(arrayLength))
+    //     console.log(arrayLength);
+    //     arrayLength--;
+    // }
+    // inputArray.forEach(function(tempboi) {
+    // });
+}
+
+function sumArray(array){
+    let arrayTotal = 0;
+    array.forEach(function(element) {
+        arrayTotal = Math.floor(element) + arrayTotal;
+    })
+    console.log(arrayTotal);
+    return arrayTotal;
+}
+
+function maxArray(array){
+    let arrayMax = 0;
+    array.forEach(function(element) {
+        if(element > arrayMax) {
+            arrayMax = Math.floor(element);
+            console.log(arrayMax, element);
+        }
+    })
+    console.log(arrayMax);
+    return arrayMax;
+}
+
+function uniform(array) {
+    // trying without a foreach
+    console.log(`starting uniform()`);
+    let arrayIndex = array[0];
+    for(var i = array.length - 1; i >= 0; i--){
+        if(array[i] !== arrayIndex) {
+            console.log(`returning false`);
+            return false;
+        }
+    }
+    console.log(`returning true`)
+    return true;
+
+    //trying this second
+    // console.log(`starting uniform()`);
+    // let arrayAnswer = true;
+    // for(var i = inputArray.length - 1; i >= 0; i--){
+    //     //check each one if it equals the next one in the array
+    //     inputArray.forEach(function(element) {
+    //         if(inputArray[i] !== element) {
+    //             arrayAnswer = false;
+    //         }
+    //     });
+    // }
+    // if (arrayAnswer === false) {
+    //     console.log(`returning false ${arrayAnswer}`);
+    //     return arrayAnswer;
+    // }
+    // console.log(`returning true ${arrayAnswer}`);
+    // return arrayAnswer;
+}
+
 // function userAnswer() {
 while (userAnswer !== `quit`) {
 userAnswer = prompt(`What would you like to do? `);
@@ -58,6 +127,20 @@ userAnswer = prompt(`What would you like to do? `);
     else if(userAnswer === `delete`) {
         //get thing to delete from user (picktoDelete)
         deleteToDo();
+    }
+    else if(userAnswer === `reverse`) {
+        //get thing to delete from user (picktoDelete)
+        printReverse(toDoArray);
+    }
+    else if(userAnswer === `uniform`) {
+        //get thing to delete from user (picktoDelete)
+        uniform(toDoArray);
+    }
+    else if(userAnswer === `add`) {
+        sumArray(toDoArray);
+    }
+    else if(userAnswer === `max`) {
+        maxArray(toDoArray);
     }
     else if(userAnswer === `quit`) {
         userAnswer = `quit`;
