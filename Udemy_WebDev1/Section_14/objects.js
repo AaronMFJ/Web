6ce04517b60680c1a268e717b4f013caf6aa0416
AaronMFJ <parasiteartist@gmail.com>
2020-04-18 16:37:00 -0400

    Finished through 14-169
    Intro to objects

--- Udemy_WebDev1/Section_14/objects.js
+++ Udemy_WebDev1/Section_14/objects.js
@@ -1,1 +1,27 @@
 console.log(`Objects.js is connected`);
+
+//example array with objects
+var posts = [
+    {
+        title: "cats are lame",
+        author: "Comet",
+        comments: ["Agreed.", "5 stars"]
+    },
+    {
+        title: "dogs are great",
+        author: "Aaron",
+        comments: ["Of course they are", "Yup"]
+    }
+]
+
+var someObjects = {
+    friends: [
+        {name: "Malfoy"},
+        {name: "Crabbie"},
+        {name: "Goyle"}
+        ],
+    color: "baby blue",
+    isEvi: true
+};
+
+//someObjects.friends[0] retrieves "Malfoy"
