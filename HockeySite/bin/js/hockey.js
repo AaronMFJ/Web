ced8f0d41280919cfdf1f97b2624ae18ecd08917
AaronMFJ <parasiteartist@gmail.com>
2020-06-25 18:27:08 -0400

    rrrr

--- HockeySite/bin/js/hockey.js
+++ HockeySite/bin/js/hockey.js
@@ -1,1 +1,28 @@
 console.log(`hockey.js is connected`);
+
+// trying to detect page user is on and set style/password
+// var currentPage = window.location.pathname;
+
+// if (currentPage == "/C:/Users/aaron/Desktop/Dev/Code/Web/HockeySite/noentry.html") {
+//     console.log("detected noentry.html");
+//     var magictag = document.querySelector("backgroundimage");
+//     magictag.classList.toggle("magic-class");
+
+// }
+// else{
+//     console.log("nothing");
+
+// };
+
+
+
+//remove rightclick ability in hopes to disable source code checking
+// function click() {
+// if (event.button==2)
+//     {   
+//         alert('This is a no right click zone');
+//     }
+// } 
+//     document.onmousedown=click
+
+    
