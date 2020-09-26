c22e2bf146b4c25202d64be9b758a7c2d6ed04e9
AaronMFJ <aaronmfj@live.com>
2020-09-26 19:16:10 -0400

    S16 189 complete

--- Udemy_WebDev1/Section_16/dom.js
+++ Udemy_WebDev1/Section_16/dom.js
@@ -12,13 +12,28 @@ console.log("dom.js is connected");
 //   });
 
 
+//  Below is two ways to change color of background with a button.
+//  The first way is classic changing the color by selecting elements
 
 var button = document.querySelector("button");
-var paragraph = document.querySelector("p");
+// var paragraph = document.querySelector("p");
+// var body = document.querySelector("body");
+// var backColor = false;
+
+//when you click the button, if backColor is even, change white, if off change blue.
+//Then add 1 to backcolor
+// button.addEventListener("click", function() {
+//     if (backColor == true) {
+//        body.style.background = "white";
+//     }
+//     else (body.style.background = "blue");
+//     backColor = !backColor; //toggle true/false
+
+// });
 
 
-button.addEventListener("click", function() {
-    button.style.background = "blue";
-    paragraph.textContent = "Yay we changed the text";
-    console.log("button clicked");
+//   Another way using class set in the HTML but toggling it on and off
+
+button.addEventListener("click", function (){
+    document.body.classList.toggle("purple");
 });
